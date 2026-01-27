e9f554a844535e845a30bcdfc2f5cbbb718dab71
glad47 <ggffhh3344@gmail.com>
2026-01-27 09:43:44 +0300

    done

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -8,33 +8,33 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def button_validate(self):
-    """
-    Force picking to done state using ONLY the qty actually received.
-    Do NOT modify qty_done or quantity_done.
-    """
-    for picking in self:
-        if picking.state in ('done', 'cancel'):
-            continue
+        """
+        Force picking to done state using ONLY the qty actually received.
+        Do NOT modify qty_done or quantity_done.
+        """
+        for picking in self:
+            if picking.state in ('done', 'cancel'):
+                continue
 
-        _logger.info('Force Done: Completing picking %s using received quantities only', picking.name)
+            _logger.info('Force Done: Completing picking %s using received quantities only', picking.name)
 
-        # Mark move lines as done WITHOUT changing qty_done
-        for move_line in picking.move_line_ids:
-            move_line.write({'state': 'done'})
+            # Mark move lines as done WITHOUT changing qty_done
+            for move_line in picking.move_line_ids:
+                move_line.write({'state': 'done'})
 
-        # Mark stock moves as done WITHOUT changing quantity_done
-        for move in picking.move_ids:
-            move.write({'state': 'done'})
+            # Mark stock moves as done WITHOUT changing quantity_done
+            for move in picking.move_ids:
+                move.write({'state': 'done'})
 
-        # Mark picking as done
-        picking.write({
-            'state': 'done',
-            'date_done': fields.Datetime.now(),
-        })
+            # Mark picking as done
+            picking.write({
+                'state': 'done',
+                'date_done': fields.Datetime.now(),
+            })
 
-        _logger.info('Force Done: Picking %s set to done', picking.name)
+            _logger.info('Force Done: Picking %s set to done', picking.name)
 
-    return True
+        return True
 
 
 
